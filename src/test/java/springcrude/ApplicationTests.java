21b05169517ac90d932e594c8a59837b49994bfb
JastonRuggy <jastonruggy@icloud.com>
2022-12-14 11:50:22 +0300

    fef

---
+++ src/test/java/springcrude/ApplicationTests.java
@@ -0,0 +1,13 @@
+package springcrude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
